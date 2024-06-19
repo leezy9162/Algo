2a3cb2a21f17435dd244fe962c9fb6ad9a557f09
leezy9162 <leezy9162@gmail.com>
2024-06-19 19:45:14 +0900

    [Bronze V] Title: A/B, Time: 216 ms, Memory: 17772 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double number1 = 0;
+        double number2 = 0;
+
+        number1 = input.nextInt();
+        number2 = input.nextInt();
+
+        System.out.println(number1 /number2 );
+
+    }
+}
