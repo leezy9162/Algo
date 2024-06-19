5f125c1806e37ee391e9f53ed508fdda45de3759
leezy9162 <leezy9162@gmail.com>
2024-06-19 19:43:35 +0900

    [Bronze V] Title: 꼬마 정민, Time: 216 ms, Memory: 17648 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        long A = input.nextLong();
+        long B = input.nextLong();
+        long C = input.nextLong();
+
+        System.out.println(A + B + C);
+
+    }
+}
