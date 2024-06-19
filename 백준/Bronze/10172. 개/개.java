10ac47f7253028f31cfbeb4e3f52add7069a07e1
leezy9162 <leezy9162@gmail.com>
2024-06-19 19:43:47 +0900

    [Bronze V] Title: 개, Time: 120 ms, Memory: 13932 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.print("|\\_/|");
+        System.out.println();
+
+        System.out.print("|q p|   /}");
+        System.out.println();
+
+        System.out.print("( 0 )\"\"\"\\");
+        System.out.println();
+
+        System.out.print("|\"^\"`    |");
+        System.out.println();
+
+        System.out.print("||_/=\\\\__|");
+    }
+}
